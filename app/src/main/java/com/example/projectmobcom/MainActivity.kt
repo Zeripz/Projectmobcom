96a1cdaa9cdc8e5dec4b1250ea1c9c5b1db12152
Zeripz <zeruknipz@gmail.com>
2023-10-29 20:59:48 +0700

    layout

--- app/src/main/java/com/example/projectmobcom/MainActivity.kt
+++ app/src/main/java/com/example/projectmobcom/MainActivity.kt
@@ -6,90 +6,69 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.projectmobcom.ui.theme.ProjectmobcomTheme
-import androidx.compose.foundation.clickable
-import android.annotation.SuppressLint
 import android.content.ActivityNotFoundException
 import android.content.Intent
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ProjectmobcomTheme {
-                Row(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(16.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween
+                Column(
+                    modifier = Modifier.fillMaxSize()
                 ) {
-                    Text(
-                        text = "Pencet tombol berikut untuk membuka second activity",
-                        modifier = Modifier.weight(1f)
-                    )
-                    Button(onClick ={
-                        Intent(applicationContext, SecondActivity::class.java).also{
-                            startActivity(it)
-                        }
-                    }){
-                        Text(text = "Secondactivity")
-                    }
-                }
-                Button(onClick ={
-                    Intent(Intent.ACTION_MAIN).also{
-                        it.`package` = "com.google.android.youtube"
-                        try {
-                            startActivity(it)
-                        }catch(e: ActivityNotFoundException){
-                            e.printStackTrace()
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            text = "Pencet tombol berikut untuk membuka second activity",
+                            modifier = Modifier.weight(1f)
+                        )
+                        Button(onClick = {
+                            Intent(applicationContext, SecondActivity::class.java).also {
+                                startActivity(it)
+                            }
+                        }) {
+                            Text(text = "Second Activity")
                         }
                     }
-                }){
-                    Text(text = "Youtube")
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            text = "Pencet tombol berikut untuk membuka Youtube",
+                            modifier = Modifier.weight(1f)
+                        )
+                        Button(onClick = {
+                            Intent(Intent.ACTION_MAIN).also {
+                                it.`package` = "com.google.android.youtube"
+                                try {
+                                    startActivity(it)
+                                } catch (e: ActivityNotFoundException) {
+                                    e.printStackTrace()
+                                }
+                            }
+                        }) {
+                            Text(text = "Youtube")
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ProjectmobcomTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/projectmobcom/SecondActivity.kt
+++ app/src/main/java/com/example/projectmobcom/SecondActivity.kt
@@ -2,29 +2,36 @@ package com.example.projectmobcom
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import com.example.projectmobcom.ui.theme.ProjectmobcomTheme
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
 
 class SecondActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent{
-            ProjectmobcomTheme{
-                Text(text = "ini halaman ke2")
-                Button(onClick ={
-                    Intent(applicationContext, MainActivity::class.java).also{
-                        startActivity(it)
+        setContent {
+            ProjectmobcomTheme {
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Button(onClick = {
+                        Intent(applicationContext, MainActivity::class.java).also {
+                            startActivity(it)
+                        }
+                    }) {
+                        Text(text = "Back")
                     }
-                }){
-                    Text(text = "Back")
+                    Text(text = "ini halaman ke-2")
                 }
             }
         }
     }
-
 }
